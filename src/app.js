63951fee976e7789cce51152ca488034168c309b
YunHa <pgup10753@gmail.com>
2024-06-22 13:12:42 +0900

    back to original style

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
-var videoContainer = document.getElementsByClassName("video-compare-container");
-var videoClipper = document.getElementsByClassName("video-clipper");
-var clippedVideo = document.getElementsByClassName("clipped-video");
-var divider = document.getElementsByClassName("divider");
+const names = document.querySelectorAll(".name");
+const univToShow = document.querySelector(".univs");
 
-function trackLocation(e) {
-    for (var i = 0; i < videoClipper.length; i++) {
-        var rect = videoContainer[i].getBoundingClientRect();
-        var position = ((e.pageX - rect.left) / videoContainer[i].offsetWidth) * 100;
-        if (position <= 100) {
-            videoClipper[i].style.width = position + "%";
-            clippedVideo[i].style.width = ((100 / position) * 100) + "%";
-            divider[i].style.left = position + "%";
-            // clippedVideo[i].style.zIndex = 3;
-        }
-    }
+
+function univWhileHovering(e) {
+    univToShow.style.display = "block";
+    univToShow.innerHTML = e.target.dataset.univ;
+    console.log(e)
+    univToShow.style.top = e.layerY+10+"px";
+    univToShow.style.left = e.layerX +10+ "px";
 }
 
-for (var i = 0; i < videoContainer.length; i++) {
-    videoContainer[i].addEventListener("mousemove", trackLocation, false);
-    videoContainer[i].addEventListener("touchstart", trackLocation, false);
-    videoContainer[i].addEventListener("touchmove", trackLocation, false);
+function hideUniv(e) {
+    univToShow.style.display = "none";
 }
 
-
-console.log(videoContainer);
+for (var i = 0; i < names.length; i++){
+    names[i].addEventListener('mousemove', univWhileHovering);
+    names[i].addEventListener('mouseleave', hideUniv)
+}
